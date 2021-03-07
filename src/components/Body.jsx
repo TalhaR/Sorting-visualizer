f45fcb0ba7a514ea5cc563de6d617f65c48493c7
Talha Rahman <39923265+TalhaR@users.noreply.github.com>
2021-03-06 22:47:31 -0500

    added quick sort visualization

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -10,6 +10,12 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function setColor(domBars, color) {
+    for (const bar of domBars) {
+        bar.style.backgroundColor = color; 
+    }
+}
+
 class Body extends React.Component {
     constructor(props) {
         super(props);
@@ -42,12 +48,13 @@ class Body extends React.Component {
 
     handleSort(algorithm) {
         this.setState({isRunning: true});
-        let array = [...this.state.elements];
+        const array = [...this.state.elements];
         switch (algorithm) {
             case 'bubble':
                 this.bubbleSort(array);
                 break;
             case 'quick':
+                this.quickSort(array);
                 break;
             case 'merge':
                 this.mergeSort(array);
@@ -60,8 +67,57 @@ class Body extends React.Component {
         }
     }
 
-    quickSort(array) {
+    async quickSort(array) {
+        const domBars = document.getElementsByClassName('bar');
 
+        const partition = async (array, start, end) => {
+            const pivotValue = array[end];
+            let pivotIndex = start;
+            setColor([domBars[end]], 'yellow');
+
+            for(let i = start; i < end; ++i) {
+                setColor([domBars[i]], 'green');
+                await sleep(20);
+
+                if (array[i].props.height <= pivotValue.props.height) {
+                    setColor([domBars[i], domBars[pivotIndex]], 'red');
+
+                    [array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+                    this.setState({elements: array});
+                    await sleep(20);
+
+                    setColor([domBars[i], domBars[pivotIndex]], 'gray');
+                    ++pivotIndex;
+                }
+                setColor([domBars[i]], 'gray');
+            }
+
+            setColor([domBars[end], domBars[pivotIndex]], 'red');
+            [array[end], array[pivotIndex]] = [array[pivotIndex], array[end]];
+            this.setState({elements: array});
+            await sleep(20);
+            setColor([domBars[end], domBars[pivotIndex]], 'gray');
+
+            return pivotIndex;
+        }
+
+        const stack = [[0, array.length - 1]];
+
+        while(stack.length !== 0) {
+            const [start, end] = stack.pop();
+
+            const pivotIndex = await partition(array, start, end);
+
+            if (pivotIndex - 1 > start) {
+                stack.push([start, pivotIndex - 1]);
+            }
+
+            if (pivotIndex + 1 < end) {
+                stack.push([pivotIndex + 1, end]);
+            }
+        }
+
+        this.setState({isRunning: false,});
     }
     
     async mergeSort(array) {
@@ -72,21 +128,17 @@ class Body extends React.Component {
             if (i % 2 === 0) {
                 const [index1, index2] = animations[i];
 
-                domBars[index1].style.backgroundColor = 'green';
-                domBars[index2].style.backgroundColor = 'green';
+                setColor([domBars[index1], domBars[index2]], 'green');
                 await sleep(25);
-                domBars[index1].style.backgroundColor = 'gray';
-                domBars[index2].style.backgroundColor = 'gray';
+                setColor([domBars[index1], domBars[index2]], 'gray');
             } else {
                 const [index1, element] = animations[i];
                 array[index1] = <Element key={'overwrite' + i} height={element.props.height} />;
                 
                 this.setState({elements: array});
-                domBars[index1].style.backgroundColor = 'red';
-                domBars[element.key].style.backgroundColor = 'red';
+                setColor([domBars[index1], domBars[element.key]], 'red');
                 await sleep(25);
-                domBars[index1].style.backgroundColor = 'gray';
-                domBars[element.key].style.backgroundColor = 'gray';
+                setColor([domBars[index1], domBars[element.key]], 'gray');
             }
         }
         this.setState({isRunning: false});
@@ -98,24 +150,21 @@ class Body extends React.Component {
         for(let i = 0; i < array.length; ++i) {
             for(let j = 0; j < array.length - 1 - i; ++j) {
                 // highlight comparing elements in green
-                domBars[j].style.backgroundColor = 'green';
-                domBars[j + 1].style.backgroundColor = 'green';
+                setColor([domBars[j], domBars[j + 1]], 'green');
                 await sleep(10);
 
                 if (array[j].props.height > array[j + 1].props.height) {
                     // highlight swapping elements red
-                    domBars[j].style.backgroundColor = 'red';
-                    domBars[j + 1].style.backgroundColor = 'red';
+                    setColor([domBars[j], domBars[j + 1]], 'red');
                     [array[j], array[j + 1]] = [array[j + 1], array[j]];
                     this.setState({elements: array,});
-                    await sleep(25);
+                    await sleep(10);
                 } 
                 // post-swap reset to normal
-                domBars[j].style.backgroundColor = 'gray';
-                domBars[j + 1].style.backgroundColor = 'gray';
+                setColor([domBars[j], domBars[j + 1]], 'gray');
             }
             // sorted elements in purple
-            domBars[array.length - 1 - i].style.backgroundColor = 'purple';
+            setColor([domBars[array.length - 1 - i]], 'purple');
         }
 
         this.setState({isRunning: false});
